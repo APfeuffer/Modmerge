80993824bc7b47e046040854d6f70f7a1591be46
Adrien Pfeuffer <adrien.pfeuffer@gmail.com>
2024-10-26 11:28:01 +0200

    First version.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Modmerge
 A simple tool for game modding: Recursively merges multiple directory trees using symlinks.
+
+## Usage
+
+python3 link_mods.py DESTINATION MODDIRS... GAMEDIR
+
+This will create new directories at DESTINATION assembled from the first file found in any of the MODDIRS, or from GAMEDIR if none is found. Directories are also linked if only one exists; otherwise, they are merged. May also be used on subdirectories. Example:
+
+- The game has all relevant assets in "data/" and your mod is named "mymod/"
+- Make sure all files you want to replace are in the proper location
+- E.g.: If the original file was "data/foo/bar.txt", yours should be "mymod/data/foo/bar.txt"
+- Make a new directory for the unmodified game files (e.g. "base/") and move "data/" there (now "base/data/")
+- Install Python 3 (if you don't have it already)
+- Copy "link_mods.py" to the game directory
+- run "python3 link_mods.py . mymod base"
+- You should now have a new data/ directory of symlinks to either your mod's or the base game's files and dirs
---
+++ link_mods.py
@@ -0,0 +1,30 @@
+import json, os, sys
+from pathlib import Path
+
+def merge(dst, *src, verbose=True):
+    if all([os.path.isfile(s) for s in src]):
+        if verbose: print(f"{dst} is a file, using first source: {src[0]}")
+        os.makedirs(Path(dst).parent,exist_ok=True)
+        os.symlink(os.path.abspath(src[0]),dst)
+    elif all([os.path.isdir(s) for s in src]):
+        if len(src)==1 and dst!='.' and dst!='..':
+            if verbose: print(f"{dst} is a directory with only one source: {src[0]}")
+            os.makedirs(Path(dst).parent,exist_ok=True)
+            os.symlink(os.path.abspath(src[0]),dst)
+        else:
+            if verbose: print(f"{dst} is a directory with multiple sources, merging recursively...")
+            contents = {}
+            for s in src:
+                cnt = list(os.listdir(s))
+                for c in cnt:
+                    if c in contents: contents[c]+=[s+'/'+c]
+                    else: contents[c]=[s+'/'+c]
+            for entry, sources in contents.items():
+                if len(dst): merge(dst+'/'+entry,*sources)
+                else: merge(entry,*sources)
+    else:
+        print(f"Merge failed. Sources must be either all files or all directories:\n{'\n'.join(src)}")
+
+if __name__=="__main__":
+    merge(*sys.argv[1:])
+
